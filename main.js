67ccad396e7754574e96ee8ca84307c5b9d3f196
David Arango <arango.david8@gmail.com>
2019-02-21 17:48:53 -0500

    User can now hover over colors to change product, added method to decrement from count on the cart

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ var app = new Vue({
 		link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
 		inventory: 100,
 		onSale: true,
+		inStock: false,
 		details: ["80% cotton", "20% polyester", "gender-neutral"],
 		variants: [
 			{
 				variantId: 2234,
 				variantColor: "green",
-				variantImage: 'assets/green-socks.jpg'
+				variantImage: 'assets/green-socks.jpg',
+				variantUrl: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0'
 			},
 			{
 				variantId: 2235,
 				variantColor: "blue",
-				variantImage: 'assets/blue-socks.jpg'
+				variantImage: 'assets/blue-socks.jpg',
+				variantUrl: 'https://www.dropbox.com/s/t32hpz32y7snfna/vmSocks-blue-onWhite.jpg?dl=0'
 			}
 		],
 		sizes: ["small", "medium", "large"],
@@ -24,10 +27,16 @@ var app = new Vue({
 	},
 	methods: {
 		addToCart() {
-			this.cart += 1;
+			this.cart++;
 		},
-		updateProduct(variantImage) {
-			this.image = variantImage; 
+		subtractFromCart() {
+			if(this.cart === 0) return;
+			this.cart--;
+
+		},
+		updateProduct(variantImage, variantUrl) {
+			this.image = variantImage;
+			this.link = variantUrl;
 		}
 	}
 });
