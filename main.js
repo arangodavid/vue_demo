76bcd3a02cef74d98474ccdb8ef0a4ca0a21ee68
David Arango <arango.david8@gmail.com>
2019-02-21 12:02:00 -0500

    Initialized repo

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'socks',
+		image: 'assets/green-socks.jpg',
+		link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
+		inStock: true
+	}
+})
