343d8e8a3953c3741f3e5023148cc437d9f8fde0
David Arango <arango.david8@gmail.com>
2019-02-21 14:21:58 -0500

    Added HTML root and children, added a vue instance with properties

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ var app = new Vue({
 		product: 'socks',
 		image: 'assets/green-socks.jpg',
 		link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
-		inStock: true
+		inventory: 100,
+		onSale: true,
+		details: ["80% cotton", "20% polyester", "gender-neutral"],
+		variants: [
+			{
+				variantId: 2234,
+				variantCOlor: "green"
+			},
+			{
+				variantId: 2235,
+				variantColor: "blue"
+			}
+		]
 	}
 })
