245d9b3fda95a91ce01759ba202872b1bf84f534
David Arango <arango.david8@gmail.com>
2019-02-22 11:46:30 -0500

    Added a computed property to vues instance, added class to cart everytime addtocart button is clicked

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		brand: "VuesaceJS",
 		product: 'socks',
 		image: 'assets/green-socks.jpg',
 		link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
 		inventory: 100,
 		onSale: true,
-		inStock: false,
+		inStock: true,
+		outOfStock: 'out-of-stock', 
+		cartClicked: false,
 		details: ["80% cotton", "20% polyester", "gender-neutral"],
 		variants: [
 			{
@@ -28,15 +31,23 @@ var app = new Vue({
 	methods: {
 		addToCart() {
 			this.cart++;
+			this.cartClicked = true;
+			setTimeout(() => {
+				this.cartClicked = false;
+			}, 500);
 		},
 		subtractFromCart() {
 			if(this.cart === 0) return;
 			this.cart--;
-
 		},
 		updateProduct(variantImage, variantUrl) {
 			this.image = variantImage;
 			this.link = variantUrl;
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product;
 		}
 	}
 });
