1acc78518170d4b2b2e0a50b78a08612ab3719ba
David Arango <arango.david8@gmail.com>
2019-02-22 15:50:15 -0500

    Minimized index.html from using binded DOM elements into a component

--- main.js
+++ main.js
@@ -1,32 +1,102 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		brand: "VuesaceJS",
-		product: 'socks',
-		image: 'assets/green-socks.jpg',
-		link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
-		inventory: 100,
-		onSale: true,
-		inStock: true,
-		outOfStock: 'out-of-stock', 
-		cartClicked: false,
-		details: ["80% cotton", "20% polyester", "gender-neutral"],
-		variants: [
-			{
-				variantId: 2234,
-				variantColor: "green",
-				variantImage: 'assets/green-socks.jpg',
-				variantUrl: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0'
-			},
-			{
-				variantId: 2235,
-				variantColor: "blue",
-				variantImage: 'assets/blue-socks.jpg',
-				variantUrl: 'https://www.dropbox.com/s/t32hpz32y7snfna/vmSocks-blue-onWhite.jpg?dl=0'
-			}
-		],
-		sizes: ["small", "medium", "large"],
-		cart: 0,
+Vue.component('productDetails', {
+	props: {
+		details: {
+			type: 
+		}
+	},
+	template: `
+		<ul class="product-detail-cont">
+			<li class="product-detail" v-for="detail in details">{{ detail }}</li>
+		</ul>
+	`
+});
+
+
+Vue.component('product', {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true
+		}
+	},
+	template: `
+		<section class="product">
+				<section class="product-image-cont">
+					<a class="product-image-link" :href="link" target="_blank">
+						<img class="product-image" :src="image" />
+					</a>	
+				</section>
+
+				<section class="product-info-cont">
+
+					<h1 class="product-info">{{ title }}</h1>
+					<span v-show="onSale" class="product-sale">{{ isSale }}</span>
+					<p v-if="inStock" class="product-count">In Stock</p>
+					<p v-else-if="inStock <= 10 && inStock > 0" class="product-count">Almost Sold Out</p>
+					<p v-else class="product-count" :class="[ !inStock ? outOfStock : 'product-count' ]">Out of Stock</p>
+					<p>Shipping is: {{ shipping }}</p>
+
+					<section class="variant-cont">
+						<section v-for="(variant, index) in variants" 
+						 :key="variant.variantId" 
+						 @mouseover="updateProduct(index, variant.variantUrl)"
+						 class="color-box"
+						 :style="{ background: variant.variantColor }"
+						 >
+						</section>
+					</section>
+
+					<section class="product-size-cont">
+						<section class="product-sizes" v-for="size in sizes">
+							<p class="product-size">{{ size }}</p>
+						</section>
+					</section>
+
+					<section class="cart-cont">
+						<button @click="addToCart" 
+								:disabled="!inStock"
+								:class="{ disabledButton: !inStock, activeButton: inStock }">Add to Cart</button>
+						<button class="remove-from-cart" @click="subtractFromCart">Remove From Cart</button>
+
+						<section :class="{ cartClicked: cartClicked, 'cart': !cartClicked }">
+							<p class="cart-count">Cart({{ cart }})</p>
+						</section>
+					</section>
+
+				</section>
+
+			</section>
+	`,
+	data() {
+		return {
+			brand: "VuesaceJS",
+			product: 'socks',
+			selectedVariant: 0,
+			link: "https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
+			inventory: 100,
+			onSale: true,
+			outOfStock: 'out-of-stock', 
+			cartClicked: false,
+			details: ["80% cotton", "20% polyester", "gender-neutral"],
+			variants: [
+				{
+					variantId: 2234,
+					variantColor: "green",
+					variantImage: 'assets/green-socks.jpg',
+					variantUrl: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
+					variantQuantity: 10
+				},
+				{
+					variantId: 2235,
+					variantColor: "blue",
+					variantImage: 'assets/blue-socks.jpg',
+					variantUrl: 'https://www.dropbox.com/s/t32hpz32y7snfna/vmSocks-blue-onWhite.jpg?dl=0',
+					variantQuantity: 0
+				}
+			],
+			sizes: ["small", "medium", "large"],
+			cart: 0,
+		} 
 	},
 	methods: {
 		addToCart() {
@@ -40,14 +110,35 @@ var app = new Vue({
 			if(this.cart === 0) return;
 			this.cart--;
 		},
-		updateProduct(variantImage, variantUrl) {
-			this.image = variantImage;
+		updateProduct(variantIndex, variantUrl) {
+			this.selectedVariant = variantIndex;
 			this.link = variantUrl;
 		}
 	},
 	computed: {
 		title() {
 			return this.brand + ' ' + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
+		},
+		isSale() {
+			if(this.onSale) return this.brand + ' ' + this.product + ' ' + 'are on Sale!'
+			return;
+		},
+		shipping() {
+			if(this.premium) return "Free";
+			return "$2.99"
 		}
+	}
+})
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		premium: false
 	}
 });
